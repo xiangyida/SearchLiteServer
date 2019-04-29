915e3ca36204a127ebf9f77b21afa7a8b813c3d8
xiangyida <3331665905@qq.com>
2019-04-29 09:18:26 +0800

    Search

--- src/main/java/xyh/lixue/entity/Problem.java
+++ src/main/java/xyh/lixue/entity/Problem.java
@@ -2,8 +2,25 @@ package xyh.lixue.entity;
 
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import sun.rmi.runtime.Log;
 
 @Data
+@Slf4j
 public class Problem {
+    //题目id
+    private int problemId;
+    //知识点
+    private String knowledgePoint;
+    //题目标题
+    private String title;
+    //出版社
+    private String publish;
+    //题目图片名字
+    private String problemPictureName;
+    //答案图片名字
+    private String answerPictureName;
+    //热度
+    private int hotPoint;
 
 }
--- src/main/java/xyh/lixue/mapper/SearchMapper.java
+++ src/main/java/xyh/lixue/mapper/SearchMapper.java
@@ -1,7 +1,12 @@
 package xyh.lixue.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
+import xyh.lixue.entity.Problem;
+
+import java.util.List;
 
 @Component
 public interface SearchMapper {
+    public List<Problem>sarchProblem(@Param("name") String name);
 }
