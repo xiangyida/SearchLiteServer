00ecbf89d747d0d5bf4a516805da9c59707ba267
xiangyida <3331665904@qq.com>
2019-05-08 16:09:43 +0800

    修改了problem使用@Data后自定义setter发生异常的bug，改实体类不再使用@Data

--- src/main/java/xyh/lixue/searchProblem/entity/Problem.java
+++ src/main/java/xyh/lixue/searchProblem/entity/Problem.java
@@ -1,7 +1,5 @@
 package xyh.lixue.searchProblem.entity;
 
-
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.annotation.Id;
@@ -15,12 +13,12 @@ import java.io.Serializable;
  * @version 2019/5/4 17:51
  * 题目信息
  */
-@Data
+
 @Document(indexName = "lixue",type = "problem")
 @PropertySource("classpath:lixue.properties")
 public class Problem implements Serializable {
-    @Value("${ocr.ui}")
-    private final String ocrUri;
+    @Value("${cos.uri}")
+    private String cosUri;
     //题目id
     @Id
     private String id;
@@ -38,12 +36,67 @@ public class Problem implements Serializable {
     //热度
     private int hotPoint;
 
-    public void setProblemPictureName(String problemPictureName){
-        this.problemPictureName=ocrUri+problemPictureName;
+    public String getCosUri() {
+        return cosUri;
     }
 
-    public void setAnswerPictureName(String answerPictureName){
-        this.answerPictureName=ocrUri+answerPictureName;
+    public void setCosUri(String cosUri) {
+        this.cosUri = cosUri;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKnowledgePoint() {
+        return knowledgePoint;
+    }
+
+    public void setKnowledgePoint(String knowledgePoint) {
+        this.knowledgePoint = knowledgePoint;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getProblemPictureName() {
+        return problemPictureName;
+    }
+
+    public void setProblemPictureName(String problemPictureName) {
+        this.problemPictureName =cosUri+problemPictureName;
+    }
+
+    public String getAnswerPictureName() {
+        return answerPictureName;
+    }
+
+    public void setAnswerPictureName(String answerPictureName) {
+        this.answerPictureName = cosUri+answerPictureName;
+    }
+
+    public int getHotPoint() {
+        return hotPoint;
+    }
+
+    public void setHotPoint(int hotPoint) {
+        this.hotPoint = hotPoint;
+    }
 }
