f5284d13eb32048faf8914e201d7853d1421764b
xiangyida <3331665905@qq.com>
2020-04-13 23:26:59 +0800

    修改

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # docker 创建网络
-docker network rm lixue_net
+docker network rm searchlite_net
 docker network create \
 --driver bridge \
 --subnet 172.12.35.0/24 \
---gateway 172.12.35.1  lixue_net
+--gateway 172.12.35.1  searchlite_net
 # 运行sql脚本
 #docker exec
 #mysql -uroot  -p123456 -Dxyh_blog</blog_init.sql
