4719eeab3a3bf7a87dd62a5a7820b7c767a49707
xiangyida <3331665905@qq.com>
2020-04-23 01:39:51 +0800

    FlinkJob 1.10.0

--- FlinkJob/src/main/resources/create_source_kafka.sql
+++ FlinkJob/src/main/resources/create_source_kafka.sql
@@ -3,15 +3,11 @@ CREATE TABLE KAFKA_SOURCE_SEARCH_DATA (
     ts TIMESTAMP(3),
     WATERMARK FOR ts as ts - INTERVAL '5' SECOND
 ) WITH (
-    'connector.type' = 'kafka',
-    'connector.version' = 'universal',
-    'connector.topic' = 'search_data',
-    'connector.startup-mode' = 'earliest-offset',
-    'connector.properties.0.key' = 'zookeeper.connect',
-    'connector.properties.0.value' = '172.12.35.21:2181',
-    'connector.properties.1.key' = 'bootstrap.servers',
-    'connector.properties.1.value' = '172.12.35.31:9092',
-    'update-mode' = 'append',
-    'format.type' = 'json',
-    'format.derive-schema' = 'true'
+    'connector.type' = 'kafka',  -- 使用 kafka connector
+    'connector.version' = 'universal',  -- kafka 版本，universal 支持 0.11 以上的版本
+    'connector.topic' = 'search_data',  -- kafka topic
+    'connector.startup-mode' = 'earliest-offset',  -- 从起始 offset 开始读取
+    'connector.properties.zookeeper.connect' = '172.12.35.21:2181',  -- zookeeper 地址
+    'connector.properties.bootstrap.servers' = '172.12.35.31:9092',  -- kafka broker 地址
+    'format.type' = 'json'  -- 数据源格式为 json
 )
