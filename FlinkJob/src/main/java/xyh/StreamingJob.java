ee937a26e8d1cafd680ec114aa27482b4d2325d9
xiangyida <3331665905@qq.com>
2020-04-28 11:30:52 +0800

    refactor

--- FlinkJob/src/main/java/xyh/StreamingJob.java
+++ FlinkJob/src/main/java/xyh/StreamingJob.java
@@ -5,9 +5,37 @@ import org.apache.flink.table.api.TableEnvironment;
 
 public class StreamingJob {
 
-	private static final String CREATE_SOURCE = "source_kafka.sql";
-	private static final String CREATE_SINK = "sink_mysql.sql";
-	private static final String OPERATOR_FREQUENCY_COUNT = "operator_frequency_count.sql";
+	private static final String CREATE_SOURCE = "CREATE TABLE KAFKA_SOURCE_SEARCH_DATA (\n" +
+			"    data VARCHAR,\n" +
+			"    ts timestamp(3),\n" +
+			"    WATERMARK FOR ts as ts - INTERVAL '5' SECOND\n" +
+			") WITH (\n" +
+			"'connector.type' = 'kafka',\n" +
+			"'connector.version' = 'universal',\n" +
+			"'connector.topic' = 'search_data',\n" +
+			"'connector.startup-mode' = 'earliest-offset',\n" +
+			"'connector.properties.zookeeper.connect' = 'localhost:2181',\n" +
+			"'connector.properties.bootstrap.servers' = 'localhost:9092',\n" +
+			"'format.type' = 'json'\n" +
+			")";
+	private static final String CREATE_SINK = "CREATE TABLE MYSQL_SINK_SEARCH_FREQUENCY (\n" +
+			"    cnt BIGINT,\n" +
+			"    cnt_time timestamp(3)\n" +
+			") WITH (\n" +
+			"    'connector.type' = 'jdbc',\n" +
+			"    'connector.url' = 'jdbc:mysql://127.0.0.1:3306/search_lite?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC',\n" +
+			"    'connector.table' = 'user_search_frequency',\n" +
+			"    'connector.username' = 'root',\n" +
+			"    'connector.password' = '123456',\n" +
+			"    'connector.write.flush.max-rows' = '1'\n" +
+			")";
+	private static final String OPERATOR_FREQUENCY_COUNT = "INSERT INTO MYSQL_SINK_SEARCH_FREQUENCY(cnt,cnt_time)\n" +
+			"SELECT\n" +
+			"COUNT(*) as cnt ,\n" +
+			"TUMBLE_START(ts, INTERVAL '1' MINUTE) as cnt_time\n" +
+			"FROM KAFKA_SOURCE_SEARCH_DATA\n" +
+			"GROUP BY TUMBLE(ts, INTERVAL '1' MINUTE)";
+
 	public static void main(String[] args) throws Exception {
 
 		EnvironmentSettings settings = EnvironmentSettings.newInstance()
@@ -17,16 +45,13 @@ public class StreamingJob {
 
 		TableEnvironment tEnv = TableEnvironment.create(settings);
 
-		String create_source_sql = FileUtil.readFileAsString(CREATE_SOURCE);
-		String create_sink_sql= FileUtil.readFileAsString(CREATE_SINK);
-		String operator_frequency_count_sql = FileUtil.readFileAsString(OPERATOR_FREQUENCY_COUNT);
 
 		//source
-		tEnv.sqlUpdate(create_source_sql);
+		tEnv.sqlUpdate(CREATE_SOURCE);
 		//sink
-		tEnv.sqlUpdate(create_sink_sql);
+		tEnv.sqlUpdate(CREATE_SINK);
 		//operator
-		tEnv.sqlUpdate(operator_frequency_count_sql);
+		tEnv.sqlUpdate(OPERATOR_FREQUENCY_COUNT);
 
 		tEnv.execute("SEARCH FREQUENCY COUNT");
 	}
