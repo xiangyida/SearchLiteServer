bacdab654e1d77a35daa615d79bbfb7a8a31efb3
xiangyida <3331665905@qq.com>
2020-05-02 14:31:44 +0800

    更改项目结构

--- build/flink.sh
+++ build/flink.sh
@@ -11,11 +11,11 @@
 #ikanalyzer-2012_u6.jar
 #flink-connector-redis_2.11-1.0.jar
 ###################################
-wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-sql-connector-kafka_2.11/1.10.0/flink-sql-connector-kafka_2.11-1.10.0.jar
-wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-json/1.10.0/flink-json-1.10.0.jar
-wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-jdbc_2.11/1.10.0/flink-jdbc_2.11-1.10.0.jar
-wget -P ./lib/ https://maven.aliyun.com/repository/public/mysql/mysql-connector-java/5.1.48/mysql-connector-java-5.1.48.jar
-wget -P ./lib/ https://maven.aliyun.com/repository/public/com/janeluo/ikanalyzer/2012_u6/ikanalyzer-2012_u6.jar
+wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-sql-connector-kafka_2.11/1.10.0/flink-sql-connector-kafka_2.11-1.10.0.jar | \
+wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-json/1.10.0/flink-json-1.10.0.jar | \
+wget -P ./lib/ https://maven.aliyun.com/repository/public/org/apache/flink/flink-jdbc_2.11/1.10.0/flink-jdbc_2.11-1.10.0.jar | \
+wget -P ./lib/ https://maven.aliyun.com/repository/public/mysql/mysql-connector-java/5.1.48/mysql-connector-java-5.1.48.jar | \
+wget -P ./lib/ https://maven.aliyun.com/repository/public/com/janeluo/ikanalyzer/2012_u6/ikanalyzer-2012_u6.jar | \
 wget -P ./lib/ https://maven.aliyun.com/repository/public/com/github/yang69/flink-connector-redis_2.11/1.0/flink-connector-redis_2.11-1.0.jar
 
 
