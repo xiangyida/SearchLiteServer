9962a0871b899301ebc179add7b62298d2b2c4b5
xiangyida <3331665905@qq.com>
2020-06-04 21:08:26 +0800

    修改docker-compose.yml，重新配置了网络环境，修改了build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 IK_PLUGIN_URL="https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v6.8.4/elasticsearch-analysis-ik-6.8.4.zip"
+MYSQL_ROOT_PASSWORD="123456"
 # 挂载目录
 VOLUMES_ROOT="/docker/"
 # 安装es插件
@@ -16,5 +17,13 @@ docker network create \
 --subnet 172.12.35.0/24 \
 --gateway 172.12.35.1  searchlite_net
 echo "已创建网络searchlite_net"
+
+# 创建容器
+docker-compose --project-name searchlite up -d
+
+# 导入mysql数据
+mysql -uroot -p$MYSQL_ROOT_PASSWORD
+source search_lite.sql
+
 
 
